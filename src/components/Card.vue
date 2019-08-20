f3b83312e50021ce323736679bcf1ee89dba5a42
Letícia <leeti@live.com>
2019-08-20 19:25:48 -0300

    :art: Create card component

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div id="cardDiv" >
+    <a> Blabla </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card"
+};
+</script>
+
+<style>
+
+#cardDiv {
+    top: 246px;
+    left: 985px;
+    width: 387px;
+    height: 283px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 0px 10px 10px #000000;
+    border: 1px solid #EBEAED;
+    border-radius: 5px;
+    opacity: 1;
+}
+
+</style>
