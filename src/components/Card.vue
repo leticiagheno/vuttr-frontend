bc667112d592d157321e4b6daf80d6bfea739cee
Letícia <leeti@live.com>
2019-08-20 22:25:43 -0300

    :construction: Work in progress

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,45 @@
 <template>
   <div class="home">
-    <div id="cardDiv" >
-    <a> Blabla </a>
+    <div id="card" >
+    <a id="name-item" v-bind:href='tool.link'> {{ tool.title }} </a>
+    <p class="text-card"> {{ tool.description }} </p>
+    <div class="row" > 
+    <p v-for="tag in tool.tags" :key="tag.id" class="text-card"> # {{ tag }} </p> 
+    </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "card"
+  name: "card-item",
+  props: ['tool']
 };
 </script>
 
 <style>
 
-#cardDiv {
-    top: 246px;
-    left: 985px;
-    width: 387px;
-    height: 283px;
+#card {
+    width: 750px;
+    height: 200px;
     background: #FFFFFF 0% 0% no-repeat padding-box;
-    box-shadow: 0px 10px 10px #000000;
+    box-shadow: 0px 5px 7px #000000;
     border: 1px solid #EBEAED;
     border-radius: 5px;
     opacity: 1;
+}
+
+#name-item {
+  text-align: left;
+}
+
+.text-card {
+  text-align: left;
+  font: Regular 20px/26px Source Sans Pro;
+  letter-spacing: 0.4px;
+  color: #170C3A;
+  opacity: 1;
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 id="page-title"> VUTTR </h1> 
+    <h2 id="page-subtitle"> Very Useful Tools To Remember </h2> 
+    <div>
+    <input id="search-item" type="search" placeholder="Digite o que está procurando..."/>
+    <input id="tags-check" type="checkbox"/> Search in tags only
+    <button> </button>
+    </div>
+    <div
+      v-for="tool in results" :key="tool.id" >
+      <Card :tool="tool"/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Card from "@/components/Card.vue";
+import axios from 'axios';
 
 export default {
-  name: "home",
+
+  name: "home-page",
   components: {
-    HelloWorld
+    Card
+  },
+  data () {
+    return {
+      results: []
+    }
+  },
+  beforeMount() {
+    var vm = this;
+    axios
+    .get('http://localhost:3000/tools')
+    .then(response => {vm.results = response.data.result})
   }
 };
 </script>
+
+<style scoped>
+
+#page-title {
+  text-align: left;
+  font: Semibold 42px/50px Source Sans Pro;
+  letter-spacing: 0.84px;
+  color: #170C3A;
+  opacity: 1;
+}
+
+#page-subtitle {
+  text-align: left;
+  font: Semibold 36px/40px Source Sans Pro;
+  letter-spacing: 0.72px;
+  color: #170C3A;
+  opacity: 1;
+}
+
+#search-item {
+  background: #F5F4F6 0% 0% no-repeat padding-box;
+  border: 1px solid #EBEAED;
+  border-radius: 5px;
+  opacity: 1;
+  width: 403px;
+  height: 50px;
+}
+
+</style>
